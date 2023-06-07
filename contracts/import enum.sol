0850687362c95bd85061488c84d0c6f26b8092ee
abhaysingh200 <tejsinghmehroli2786@gmail.com>
2023-06-07 22:57:07 +0530

    new 7-06

---
+++ contracts/import enum.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract importenum{
+
+    enum status{
+        pending,shipped,accepted,rejected,canceled
+    }
+
+    status public maan;
+
+}
