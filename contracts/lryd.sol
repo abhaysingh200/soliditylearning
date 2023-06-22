e1555e7c7f4b610db26cdbd4a0049bb66f3de41c
abhaysingh200 <tejsinghmehroli2786@gmail.com>
2023-06-22 09:50:34 +0530

    22-06

---
+++ contracts/lryd.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IERC20 {
+    function transfer(address , uint )external;  
+}
+
+contract Token{
+    function transfer(address , uint) external {
+
+    }
+}
